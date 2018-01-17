b0f9531e40730b504d5c82e88e22f77490899d02
0914ds <il>
2018-01-17 16:46:10 +0800

    hystrix+ribbon

--- cloud-simple-service/src/main/java/cloud/simple/service/SimpleApplication.java
+++ cloud-simple-service/src/main/java/cloud/simple/service/SimpleApplication.java
@@ -3,8 +3,10 @@ package cloud.simple.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
 public class SimpleApplication {
 
 	public static void main(String[] args) {
