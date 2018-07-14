243d2a2c982925d3e9f5fa511e8a5d0e35633ba3
0914ds <il>
2018-07-14 11:34:57 +0800

    add config project

---
+++ cloud-config-server/src/main/java/cloud/config/cloudconfigserver/CloudConfigServerApplication.java
@@ -0,0 +1,12 @@
+package cloud.config.cloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServerApplication.class, args);
+	}
+}
